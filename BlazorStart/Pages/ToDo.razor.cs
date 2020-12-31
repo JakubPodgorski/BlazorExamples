de85fb128500fadc2804ac6dcc3e70b64d184754
jakub_podgorski <jakub.podgorski@pl.abb.com>
2020-12-31 12:53:50 +0100

    Added project

---
+++ BlazorStart/Pages/ToDo.razor.cs
@@ -0,0 +1,29 @@
+﻿using BlazorStart.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStart.Pages
+{
+    public partial class ToDo
+    {
+        private string newTodo;
+        private IList<TodoItem> todos = new List<TodoItem>();
+
+        protected override void OnInitialized()
+        {
+            todos.Add(new TodoItem() { Title = "Shopping", IsDone = false });
+            todos.Add(new TodoItem() { Title = "Call", IsDone = true });
+        }
+
+        private void AddTodo()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new TodoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+    }
+}
---
+++ BlazorStart/wwwroot/js/JSInteropExample.js
@@ -0,0 +1,19 @@
+﻿window.exampleJSFunctions = {
+
+    showPrompt: function (text) {
+        return prompt(text, 'Type your name here');
+    },
+
+    displayWelcome: function (welcomeMessage) {
+        document.getElementById('welcome').innerText = welcomeMessage;
+    },
+
+    showAlert: function () {
+        alert('Hello from JS');
+    },
+
+    sayHelloCallerJS: function () {
+        DotNet.invokeMethodAsync('BlazorStart', 'SayHello', 'Blazor presentation');
+}
+
+};
